a77e58bbbbd8e7ac5a52f70bff70e2a99a47fe3a
Ruok233 <i@ruok.cc>
2021-04-04 22:25:01 +0800

    略

--- src/cc/ruok/nukkitpanel/Main.java
+++ src/cc/ruok/nukkitpanel/Main.java
@@ -49,7 +49,7 @@ public class Main extends PluginBase {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+             e.printStackTrace();
         }
     }
 
