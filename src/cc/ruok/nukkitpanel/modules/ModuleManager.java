c3a5d60df2fc72e41e873c762a9b6a9063d9ff1c
Ruok233 <i@ruok.cc>
2021-04-04 22:26:07 +0800

    添加方法

--- src/cc/ruok/nukkitpanel/modules/ModuleManager.java
+++ src/cc/ruok/nukkitpanel/modules/ModuleManager.java
@@ -1,4 +1,26 @@
 package cc.ruok.nukkitpanel.modules;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModuleManager {
+
+    private static ModuleManager manager = new ModuleManager();
+
+    private Map<String, Module> modules = new HashMap<>();
+
+    private ModuleManager() {}
+
+    public static ModuleManager getInstance() {
+        return manager;
+    }
+
+    public void register(String id, Module module) {
+        modules.put(id, module);
+    }
+
+    public Module getModules(String id) {
+        return modules.get(id);
+    }
+
 }
