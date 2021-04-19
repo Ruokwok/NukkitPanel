5677b232f36ca032e663fd8f4192c6faf62cec73
Administrator <i@ruok.cc>
2021-04-19 22:30:49 +0800

    完善了注册方法并引入异常

--- src/cc/ruok/nukkitpanel/modules/ModuleManager.java
+++ src/cc/ruok/nukkitpanel/modules/ModuleManager.java
@@ -1,13 +1,18 @@
 package cc.ruok.nukkitpanel.modules;
 
-import java.util.HashMap;
-import java.util.Map;
+import cc.ruok.nukkitpanel.R;
+import cc.ruok.nukkitpanel.modules.exception.ModuleExecption;
+import cc.ruok.nukkitpanel.modules.exception.ModuleIsExistedExecption;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModuleManager {
 
     private static ModuleManager manager = new ModuleManager();
 
-    private Map<String, Module> modules = new HashMap<>();
+    private List<Module> modules = new ArrayList<>();
 
     private ModuleManager() {}
 
@@ -15,12 +20,47 @@ public class ModuleManager {
         return manager;
     }
 
-    public void register(String id, Module module) {
-        modules.put(id, module);
+    public boolean exists(String id) {
+        boolean e = false;
+        for (int i = 0; i < modules.size(); i++) {
+            if (modules.get(i).getId().equals(id)) {
+                e = true;
+            }
+            break;
+        }
+        return e;
+    }
+
+    public void register(Module module) throws ModuleExecption {
+        if (exists(module.getId())) {
+            throw new ModuleIsExistedExecption(module.getId());
+        }
+        modules.add(module);
+        try {
+            R.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Module getModules(String id) {
-        return modules.get(id);
+        Module module = null;
+        for (int i = 0; i < modules.size(); i++) {
+            Module select = modules.get(i);
+            if (select.getId().equals(id)) {
+                module = select;
+                break;
+            }
+        }
+        return module;
+    }
+
+    public String getDrawerHtml() {
+        StringBuilder d = new StringBuilder();
+        for (int i = 0; i < modules.size(); i++) {
+            d.append(modules.get(i).getDrawerHtml());
+        }
+        return d.toString();
     }
 
 }
