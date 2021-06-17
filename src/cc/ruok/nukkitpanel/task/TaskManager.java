418bb1aef6528396dffb00198f81be9a5e73b961
Administrator <i@ruok.cc>
2021-06-17 22:21:28 +0800

    计划任务

--- src/cc/ruok/nukkitpanel/task/TaskManager.java
+++ src/cc/ruok/nukkitpanel/task/TaskManager.java
@@ -2,8 +2,11 @@ package cc.ruok.nukkitpanel.task;
 
 import cc.ruok.nukkitpanel.Config;
 import cc.ruok.nukkitpanel.Main;
+import cn.nukkit.utils.Logger;
 import com.google.gson.Gson;
 import org.apache.commons.io.FileUtils;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,12 +14,63 @@ import java.io.IOException;
 public class TaskManager {
 
     private static TaskList list;
+    private static Logger logger = Main.getInstance().getLogger();
 
     public static void load() {
         try {
             String json = FileUtils.readFileToString(new File(Main.getInstance().getDataFolder().getPath() + "/tasks.json"), Config.getCharset());
             list = new Gson().fromJson(json, TaskList.class);
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean add(Task task) {
+        try {
+            list.add(task);
+            list.save();
+            quartz(list.list.size() - 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            list.list.remove(list.list.size() - 1);
+            list.save();
+            return false;
+        }
+        return true;
+    }
+
+    public static void update() {
+        for (int i = 0; i < list.list.size(); i++) {
+            try {
+                quartz(i);
+            } catch (Exception e) {
+                logger.warning("Cron表达式无效: " + list.list.get(i).expression + " /" + list.list.get(i).title);
+            }
+        }
+    }
+
+    public static void quartz(int i) throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        JobDetail job = JobBuilder.newJob(TaskJob.class).withIdentity(String.valueOf(i), "base").build();
+        CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity(String.valueOf(i),
+                "base").withSchedule(CronScheduleBuilder.cronSchedule(list.list.get(i).expression)).build();
+        scheduler.scheduleJob(job,trigger);
+        scheduler.start();
+    }
+
+    public static TaskList getList() {
+        return list;
+    }
+
+    public static void removeJob(String name) {
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        try {
+            Scheduler sched = schedulerFactory.getScheduler();
+            TriggerKey triggerKey = TriggerKey.triggerKey(name, "base");
+            sched.pauseTrigger(triggerKey);// 停止触发器
+            sched.unscheduleJob(triggerKey);// 移除触发器
+            sched.deleteJob(JobKey.jobKey(name, "base"));// 删除任务
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
