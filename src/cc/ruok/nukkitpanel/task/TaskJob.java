ecb3cdc39772c68da38fad7a61164da2ca1fc351
Administrator <i@ruok.cc>
2021-06-17 21:48:11 +0800

    定时任务

---
+++ src/cc/ruok/nukkitpanel/task/TaskJob.java
@@ -0,0 +1,32 @@
+package cc.ruok.nukkitpanel.task;
+
+import cc.ruok.nukkitpanel.Main;
+import cn.nukkit.Server;
+import cn.nukkit.command.ConsoleCommandSender;
+import cn.nukkit.scheduler.NukkitRunnable;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.ArrayList;
+
+public class TaskJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        int i = Integer.parseInt(jobExecutionContext.getTrigger().getJobKey().getName());
+        ArrayList<Task> list = TaskManager.getList().list;
+        String[] command = list.get(i).command;
+        Main.getInstance().getLogger().info("执行计划任务: " + list.get(i).title);
+        for (int j = 0; j < command.length; j++) {
+            int o = j;
+            new NukkitRunnable() {
+                @Override
+                public void run() {
+                    Server.getInstance().dispatchCommand(new ConsoleCommandSender(), command[o]);
+                }
+            }.runTask(Main.getInstance());
+        }
+    }
+
+}
