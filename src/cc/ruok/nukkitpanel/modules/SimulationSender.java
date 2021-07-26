4f7e554c5ed161593d7adc4fa9fa45923aadce43
Ruokwok <i@ruok.cc>
2021-07-26 16:28:40 +0800

    新增 消息输出到socket对象

--- src/cc/ruok/nukkitpanel/modules/SimulationSender.java
+++ src/cc/ruok/nukkitpanel/modules/SimulationSender.java
@@ -1,11 +1,21 @@
 package cc.ruok.nukkitpanel.modules;
 
+import cc.ruok.nukkitpanel.api.json.TipsJson;
 import cn.nukkit.command.ConsoleCommandSender;
+import org.java_websocket.WebSocket;
 
 public class SimulationSender extends ConsoleCommandSender {
 
+    private WebSocket socket;
+
+    public SimulationSender(WebSocket socket) {
+        this.socket = socket;
+    }
+
     @Override
     public void sendMessage(String message) {
-        System.out.println(message);
+        try {
+            socket.send(new TipsJson(message.replaceAll("§.", "")).toString());
+        } catch (Exception e) {}
     }
 }
