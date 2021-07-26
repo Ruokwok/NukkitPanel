ded239e29fc18ab2cd7ff061bd0ea02bbdb79d2e
Ruokwok <i@ruok.cc>
2021-07-26 16:30:00 +0800

    创建 使用BN命令类

---
+++ src/cc/ruok/nukkitpanel/api/json/BNCommandJson.java
@@ -0,0 +1,13 @@
+package cc.ruok.nukkitpanel.api.json;
+
+public class BNCommandJson extends Json {
+
+    public BNCommandJson() {
+        type = "MD_BN_CMD";
+    }
+
+    public String param;
+
+    public String plugin;
+
+}
