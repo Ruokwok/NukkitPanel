d28dfb6aa4c4267376194c380e44ebdea46536b3
Ruokwok <i@ruok.cc>
2021-08-11 22:31:01 +0800

    创建 bpm仓库操作类

---
+++ src/cc/ruok/nukkitpanel/api/json/BpmCheckJson.java
@@ -0,0 +1,15 @@
+package cc.ruok.nukkitpanel.api.json;
+
+public class BpmCheckJson extends Json {
+
+    public BpmCheckJson() {
+        type = "BPM_CHECK";
+    }
+
+    public String plugin;
+
+    public String version;
+
+    public String newest;
+
+}
